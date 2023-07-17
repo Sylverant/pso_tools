e8daf92160ac94a475dc504caee27b5d17638a9b
Lawrence Sebald <ljsebald@users.sourceforge.net>
2023-07-16 23:21:21 -0400

    Fix the xboxdlqconv tool.

--- xboxdlqconv/xboxdlqconv.c
+++ xboxdlqconv/xboxdlqconv.c
@@ -1,7 +1,7 @@
 /*
     Sylverant PSO Tools
     Xbox Download Quest Converter
-    Copyright (C) 2021 Lawrence Sebald
+    Copyright (C) 2021, 2023 Lawrence Sebald
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License version 3
@@ -79,10 +79,23 @@ typedef struct xbox_quest_file {
     uint32_t unused2;
 } PACKED xbox_quest_file_pkt;
 
-static int write_xbox_hdr(FILE *fp, gc_quest_file_pkt *gc, uint16_t quest_id,
+typedef struct dc_quest_chunk {
+    dc_pkt_hdr_t hdr;
+    char filename[16];
+    char data[1024];
+    uint32_t length;
+} PACKED dc_quest_chunk_pkt;
+
+static int write_xbox_hdr(FILE *fp, gc_quest_file_pkt *gc,
+                          unsigned long quest_id, unsigned long ep,
                           char lang) {
     xbox_quest_file_pkt xb;
     int isbin = 0;
+
+    if(ep == 2)
+        ep = 256;
+    else
+        ep = 0;
 
     if(strstr(gc->filename, ".bin"))
         isbin = 1;
@@ -93,18 +106,54 @@ static int write_xbox_hdr(FILE *fp, gc_quest_file_pkt *gc, uint16_t quest_id,
     xb.hdr.pkt_len = LE16(0x0054);
 
     memcpy(xb.name, gc->name, 32);
-    xb.quest_id = LE16(quest_id);
+    xb.quest_id = LE16(quest_id | ep);
     if(isbin)
-        sprintf(xb.filename, "quest%d.bin", quest_id);
+        sprintf(xb.filename, "quest%lu.bin", quest_id | ep);
     else
-        sprintf(xb.filename, "quest%d.dat", quest_id);
+        sprintf(xb.filename, "quest%lu.dat", quest_id | ep);
     xb.length = gc->length;
 
-    sprintf(xb.xbox_filename, "quest%d_%c.dat", quest_id, lang);
-    xb.quest_id2 = LE16(quest_id);
+    sprintf(xb.xbox_filename, "quest%lu_%c.dat", quest_id | ep, lang);
+    xb.quest_id2 = LE16(quest_id | ep);
     xb.flags2 = LE16(0x3000);
 
     return fwrite(&xb, sizeof(xbox_quest_file_pkt), 1, fp);
+}
+
+static int copy_chunks(FILE *in, FILE *out, unsigned long qid,
+                       unsigned long ep) {
+    dc_quest_chunk_pkt pkt;
+    char fn[17];
+    uint16_t len;
+
+    if(ep == 2)
+        ep = 256;
+    else
+        ep = 0;
+
+    memset(fn, 0, sizeof(fn));
+
+    while(fread(&pkt.hdr, 1, sizeof(dc_pkt_hdr_t), in) == sizeof(dc_pkt_hdr_t)) {
+        len = LE16(pkt.hdr.pkt_len) - sizeof(dc_pkt_hdr_t);
+        if(fread(pkt.filename, 1, len, in) != (ssize_t)len) {
+            perror("Error reading file data");
+            return -1;
+        }
+
+        memcpy(fn, pkt.filename, 16);
+        if(strstr(fn, ".bin"))
+            sprintf(pkt.filename, "quest%lu.bin", qid | ep);
+        else
+            sprintf(pkt.filename, "quest%lu.dat", qid | ep);
+
+        len += sizeof(dc_pkt_hdr_t);
+        if(fwrite(&pkt, 1, len, out) != (ssize_t)len) {
+            perror("Error copying file data");
+            return -1;
+        }
+    }
+
+    return 0;
 }
 
 static int copy_data(FILE *in, FILE *out) {
@@ -125,39 +174,51 @@ int main(int argc, char *argv[]) {
     FILE *in, *out;
     gc_quest_file_pkt gc;
     char lang;
-    uint16_t qid;
+    unsigned long qid, ep;
     int rv;
 
-    if(argc != 5) {
-        printf("Usage: %s input output quest_id l\n", argv[0]);
+    if(argc != 6) {
+        printf("Usage: %s input output quest_id episode l\n", argv[0]);
         printf("Where l is letter representing a language (j, e, f, s, g)\n");
         return 1;
     }
 
     errno = 0;
-    qid = (uint16_t)strtoul(argv[3], NULL, 0);
+    qid = strtoul(argv[3], NULL, 0);
     if(errno) {
         perror("Cannot read quest id");
         return 1;
     }
 
-    if(qid > 999) {
-        printf("Quest ID '%s' is invalid, must be less than 1000.", argv[3]);
+    if(qid > 255) {
+        printf("Quest ID '%s' is invalid, must be less than 255.", argv[3]);
         return 1;
     }
 
-    if(strlen(argv[4]) != 1) {
+    errno = 0;
+    ep = strtoul(argv[4], NULL, 0);
+    if(errno) {
+        perror("Cannot read episode");
+        return 1;
+    }
+
+    if(ep != 1 && ep != 2) {
+        printf("Episode '%s' is invalid, must be 1 or 2.", argv[4]);
+        return 1;
+    }
+
+    if(strlen(argv[5]) != 1) {
         printf("Language code '%s' is invalid\n", argv[4]);
         return 1;
     }
 
-    if(argv[4][0] != 'j' && argv[4][0] != 'e' && argv[4][0] != 'f' &&
-       argv[4][0] != 's' && argv[4][0] != 'g') {
-        printf("Language code '%s' is invalid\n", argv[4]);
+    if(argv[5][0] != 'j' && argv[5][0] != 'e' && argv[5][0] != 'f' &&
+       argv[5][0] != 's' && argv[5][0] != 'g') {
+        printf("Language code '%s' is invalid\n", argv[5]);
         return 1;
     }
 
-    lang = argv[4][0];
+    lang = argv[5][0];
 
     if(!(in = fopen(argv[1], "rb"))) {
         perror("Cannot open input file");
@@ -177,7 +238,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    if(write_xbox_hdr(out, &gc, qid, lang) != 1) {
+    if(write_xbox_hdr(out, &gc, qid, ep, lang) != 1) {
         printf("Cannot write to output file\n");
         fclose(out);
         fclose(in);
@@ -191,14 +252,14 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    if(write_xbox_hdr(out, &gc, qid, lang) != 1) {
+    if(write_xbox_hdr(out, &gc, qid, ep, lang) != 1) {
         printf("Cannot write to output file\n");
         fclose(out);
         fclose(in);
         return 1;
     }
 
-    rv = copy_data(in, out);
+    rv = copy_chunks(in, out, qid, ep);
     fclose(out);
     fclose(in);
 
